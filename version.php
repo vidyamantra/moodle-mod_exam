5c951bb8ca34cb7d4946def80f76ebbb9418d8ec
Jai Gupta <mail@jaigupta.com>
2014-11-23 16:42:05 +0530

    plugin notation

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2014110701;         // The current module version (Date: YYYYMMDDXX)
-$module->release = '1.0 (Build: 2014112000)';
-$module->requires  = 2014051200;      // Requires this Moodle version(2.7+)
-$module->cron      = 0;               // Period for cron to check this module (secs)
-$module->maturity = MATURITY_STABLE;
-$module->component = 'mod_exam'; 
+$plugin->version   = 2014110701;         // The current module version (Date: YYYYMMDDXX)
+$plugin->release = '1.0 (Build: 2014112000)';
+$plugin->requires  = 2014051200;      // Requires this Moodle version(2.7+)
+$plugin->cron      = 0;               // Period for cron to check this module (secs)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->component = 'mod_exam'; 
