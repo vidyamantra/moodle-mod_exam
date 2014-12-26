c5aa0288162f5a7418d74fb352f9c030f0c9c506
Pinky Sharma <pinky@vidyamantra.com>
2014-12-26 11:50:18 +0530

    Espanol lang support added

---
+++ lang/es_mx/exam.php
@@ -0,0 +1,51 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Español - México strings for exam module
+ * @package   mod
+ * @subpackage exam
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['attemptallowed'] = 'Intentos permitidos: {$a}';
+$string['attempted'] = 'Intentado: {$a}';
+$string['attemptexamnow'] = 'Intentar EXAMEN ahora';
+$string['cachedef_examdata'] = 'Datos de preguntas del EXAMEN';
+$string['donothatvepermissiontoattempt'] = 'Usted no tiene permiso para intentar este EXAMEN.';
+$string['eventexamattempted'] = 'EXAMEN intentado';
+$string['exam'] = 'EXAMEN';
+$string['exam:addinstance'] = 'Añadir un nuevo EXAMEN';
+$string['exam:attempt'] = 'Enviar EXAMEN';
+$string['exam:preview'] = 'Previsualizar EXAMEN';
+$string['exam:view'] = 'Ver EXAMEN';
+$string['examfieldset'] = 'Conjunto_de_campo personalizado de ejemplo';
+$string['examname'] = 'Nombre del EXAMEN';
+$string['getstarted'] = '¡Empiece!';
+$string['gradeset'] = 'Calificación';
+$string['invalidexamid'] = 'ID del examen inválida';
+$string['marksperquestion'] = 'Puntos para cada pregunta';
+$string['maxpoint'] = 'Puntos máximos';
+$string['modulename'] = 'EXAMEN';
+$string['modulename_help'] = 'El módulo de EXAMEN le permite a los usuarios tomar exámenes de preguntas de opción múltiple basados en exámenes seleccionados . 
+
+Aviso sobre Seguridad:
+Este módulo contiene código JavaScript en el navegador. Técnicamente es posible que los estudiantes manipulen sus calificaciones del EXAMEN si sben los parámetros correctos. Use con precaución.';
+$string['modulenameplural'] = 'EXÁMENES';
+$string['noquiz'] = 'No hay Examen para el EXAMEN.';
