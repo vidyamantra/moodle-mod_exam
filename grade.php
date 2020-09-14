59fdfcb8662958a12aae4289a8911d35f2f2b4e6
Germán Valero <gvalero@unam.mx>
2020-09-14 13:05:04 -0500

    Line exceeds 132 characters; contains 140 characters

--- grade.php
+++ grade.php
@@ -26,7 +26,8 @@
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 
 $id = required_param('id', PARAM_INT);          // Course module ID
-$itemnumber = optional_param('itemnumber', 0, PARAM_INT); // Item number, may be != 0 for activities that allow more than one grade per user
+$itemnumber = optional_param('itemnumber', 0, PARAM_INT);
+// Item number, may be != 0 for activities that allow more than one grade per user
 $userid = optional_param('userid', 0, PARAM_INT); // Graded user ID (optional).
 
 if ($id) {
